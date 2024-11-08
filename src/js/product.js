98709289a68dda4120b62b10da3709323fef3a0b
migarcos <migark@hotmail.com>
2024-11-08 09:49:08 -0500

    indiviudal1 task

--- src/js/product.js
+++ src/js/product.js
@@ -1,10 +1,31 @@
-import { setLocalStorage } from "./utils.mjs";
+import { setLocalStorage, getLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 
 const dataSource = new ProductData("tents");
 
 function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
+  // setLocalStorage("so-cart", product);
+
+  // call the array info from LocalStorage using and utils function.
+  let cart = getLocalStorage("so-cart");
+
+  if (!Array.isArray(cart)) {
+    cart = [];
+  }
+  
+  // const inCarProd = cart.find((item) => item.Id === product.Id);
+
+  // if (inCarProd) {
+  //   inCarProd.Quantity += 1;
+  // } else {
+  //   product.Quantity = 1;
+
+  //   cart.push(product);
+  // }
+
+  cart.push(product); // add individual products 
+
+  setLocalStorage("so-cart", cart);
 }
 // add to cart button event handler
 async function addToCartHandler(e) {
