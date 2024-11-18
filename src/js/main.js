005e35ff851d22ae584df9c298556e224c953104
David Labra Gaona <davidlabragaona@gmail.com>
2024-11-18 17:34:31 -0300

    Dynamic product list

---
+++ src/js/main.js
@@ -0,0 +1,8 @@
+import ProducData from './ProductData.mjs';
+import ProductList from './ProductList.mjs';
+
+const dataSource = new ProducData('tents');
+const element = document.querySelector('.product-list');
+const listing = new ProductList('Tents', dataSource, element);
+
+listing.init();
