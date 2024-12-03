2df145348a65614aaa69431251843ec2a8c167a5
angelusa <boatkayili@gmail.com>
2024-12-03 12:15:00 -0800

    Update Total$ In Cart.

--- src/js/ProductDetails.mjs
+++ src/js/ProductDetails.mjs
@@ -1,6 +1,6 @@
-import { setLocalStorage } from './utils.mjs';
+import { setLocalStorage, getLocalStorage } from './utils.mjs';
 
-export default class ProducDetails {
+export default class ProductDetails {
     constructor(productId, dataSource) {
         this.productId = productId;
         this.product = {};
@@ -8,19 +8,28 @@ export default class ProducDetails {
     }
 
     async init() {
-      // use our datasource to get the details for the current product. findProductById will return a promise! use await or .then() to process it
-      this.product = await this.dataSource.findProductById(this.productId);
-      // once we have the product details we can render out the HTML
-      this.renderProductDetails('main');
-      // once the HTML is rendered we can add a listener to Add to Cart button
-      // Notice the .bind(this). Our callback will not work if we don't include that line. Review the readings from this week on 'this' to understand why.
-      document
-          .getElementById('addToCart')
-          .addEventListener('click', this.addToCart.bind(this));
+        // Use our datasource to get the details for the current product
+        this.product = await this.dataSource.findProductById(this.productId);
+        // Render the product details
+        this.renderProductDetails('main');
+        // Add a listener to the Add to Cart button
+        document
+            .getElementById('addToCart')
+            .addEventListener('click', this.addToCart.bind(this));
     }
 
-    addToCart(product) {
-        setLocalStorage('so-cart', this.product);
+    addToCart() {
+        // Retrieve the current cart from local storage
+        let cart = getLocalStorage('so-cart') || [];
+
+        // Add the current product to the cart
+        cart.push(this.product);
+
+        // Save the updated cart back to local storage
+        setLocalStorage('so-cart', cart);
+
+        // Provide feedback to the user
+        alert('Product added to cart!');
     }
 
     renderProductDetails(selector) {
@@ -55,5 +64,5 @@ function productDetailsTemplate(product) {
         <div class="product-detail__add">
           <button id="addToCart" data-id="${product.Id}">Add to Cart</button>
         </div>
-      </section>`
+      </section>`;
 }
--- src/js/cart.js
+++ src/js/cart.js
@@ -1,30 +1,30 @@
 import { getLocalStorage } from './utils.mjs';
 
 function renderCartContents() {
-  const cartItems = getLocalStorage('so-cart');
-  if (!cartItems) {
-    return;
-  }
-  const htmlItems = cartItems.map((item) => cartItemTemplate(item));
-  document.querySelector('.product-list').innerHTML = htmlItems.join('');
-  updateCartTotal(cartItems);
+    const cartItems = getLocalStorage('so-cart');
+    if (!cartItems) {
+        return;
+    }
+    const htmlItems = cartItems.map((item) => cartItemTemplate(item));
+    document.querySelector('.product-list').innerHTML = htmlItems.join('');
+    updateCartTotal(cartItems);
 }
 
 function cartItemTemplate(item) {
-  const newItem = `<li class='cart-card divider'>
-  <a href='#' class='cart-card__image'>
-    <img
-      src='${item.Image}'
-      alt='${item.Name}'
-    />
-  </a>
-  <a href='#'>
-    <h2 class='card__name'>${item.Name}</h2>
-  </a>
-  <p class='cart-card__color'>${item.Colors[0].ColorName}</p>
-  <p class='cart-card__quantity'>qty: 1</p>
-  <p class='cart-card__price'>$${item.FinalPrice}</p></li>`;  return newItem;}function updateCartTotal(cartItems) {  const cartFooter = document.querySelector('.cart-footer');  const cartTotalElement = document.querySelector('.cart-total');  if (!cartItems || cartItems.length === 0) {    cartFooter.classList.add('hide');    return;  }  const total = cartItems.reduce((sum, item) => sum + item.FinalPrice, 0);  cartTotalElement.textContent = `Total: $${total.toFixed(2)}`;
-  cartFooter.classList.remove('hide');
+    return `<li class='cart-card divider'>
+        <a href='#' class='cart-card__image'>
+            <img
+                src='${item.Image}'
+                alt='${item.Name}'
+            />
+        </a>
+        <a href='#'>
+            <h2 class='card__name'>${item.Name}</h2>
+        </a>
+        <p class='cart-card__color'>${item.Colors[0].ColorName}</p>
+        <p class='cart-card__quantity'>qty: 1</p>
+        <p class='cart-card__price'>$${item.FinalPrice}</p>    </li>`;}function updateCartTotal(cartItems) {    const cartFooter = document.querySelector('.cart-footer');    const cartTotalElement = document.querySelector('.cart-total');    // Hide total if cart is empty    if (!cartItems || cartItems.length === 0) {        cartFooter.classList.add('hide');        return;    }    // Calculate total of all items    const total = cartItems.reduce((sum, item) => sum + item.FinalPrice, 0);    // Display total with 2 decimal places    cartTotalElement.textContent = `Total: $${total.toFixed(2)}`;
+    cartFooter.classList.remove('hide');
 }
 
 renderCartContents();
