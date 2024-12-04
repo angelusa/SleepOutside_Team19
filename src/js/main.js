fbbbf88bcc38d568545a679352bef329aadb6367
angelusa <boatkayili@gmail.com>
2024-12-03 23:55:42 -0800

    Restore files and update code changes

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,7 @@
-import ProductData from './ProductData.mjs';
+import ExternalService from './ExternalServices.mjs';
 import ProductList  from './ProductList.mjs';
 
-const dataSource = new ProductData('tent');
+const dataSource = new ExternalService('tent');
 const element = document.querySelector('.product-list');
 const listing = new ProductList('Tents', dataSource, element);
 
--- src/js/product.js
+++ src/js/product.js
@@ -1,16 +1,16 @@
-import { setLocalStorage, getLocalStorage } from "./utils.mjs";
-import ProductData from "./ProductData.mjs";
+import { setLocalStorage, getLocalStorage } from './utils.mjs';
+import ExternalServices from './ExternalServices.mjs';
 
-const dataSource = new ProductData("tents");
+const dataSource = new ExternalServices('tents');
 
 function addProductToCart(product) {
 
-  let cart = getLocalStorage("so-cart") || [];
+  let cart = getLocalStorage('so-cart') || [];
   if (!Array.isArray(cart)) {
     cart = [];
   }
   cart.push(product);
-  setLocalStorage("so-cart", cart);
+  setLocalStorage('so-cart', cart);
   updateCartCount(); // Update the cart count after adding a product
 }
 
@@ -21,12 +21,12 @@ async function addToCartHandler(e) {
 }
 
 function updateCartCount() {
-  const cart = getLocalStorage("so-cart") || [];
-  const cartCountElement = document.querySelector(".cart-count");
+  const cart = getLocalStorage('so-cart') || [];
+  const cartCountElement = document.querySelector('.cart-count');
   if (cartCountElement) {
     cartCountElement.textContent = cart.length;
   }
 }
 // Agrega el evento "click" al botón "Add to Cart"
-document.getElementById("addToCart").addEventListener("click", addToCartHandler);
+document.getElementById('addToCart').addEventListener('click', addToCartHandler);
 updateCartCount(); // Ensure count is updated when page loads
