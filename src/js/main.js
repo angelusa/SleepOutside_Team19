f200981de27d7f5a6a902dce8660b4bd15cd40cb
David Labra Gaona <davidlabragaona@gmail.com>
2024-12-10 07:11:16 -0300

    w03

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,12 @@
 import ProducData from './ProductData.mjs';
 import ProductList from './ProductList.mjs';
+import { loadHeaderFooter } from './utils.mjs';
+
+loadHeaderFooter();
 
 const dataSource = new ProducData('tents');
 const element = document.querySelector('.product-list');
 const listing = new ProductList('Tents', dataSource, element);
 
 listing.init();
+
--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -35,3 +35,26 @@ export function renderListWithTemplate(templateFn, parentElement, list, position
     parentElement.innerHTML = '';
   parentElement.insertAdjacentHTML(position, htmlStrings.join(''));
 }
+
+export function renderWithTemplate(template, parent, data, callback) {
+  parent.insertAdjacentHTML('afterbegin', template);
+  if (callback) {
+    callback(data);
+  }
+}
+
+export async function loadHeaderFooter() {
+  const headerTemplate = await loadTemplate('../partials/header.html');
+  const headerElement = document.querySelector("#main-header");
+  const footerTemplate = await loadTemplate('../partials/footer.html');
+  const footerElement = document.querySelector("#main-footer");
+
+  renderWithTemplate(headerTemplate, headerElement);
+  renderWithTemplate(footerTemplate, footerElement)
+}
+
+async function loadTemplate(path) {
+  const res = await fetch(path);
+  const template = res.text();//document.createElement('template');
+  return template;
+}
